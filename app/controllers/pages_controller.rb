0546ba59f0cae1ae003206f20d16f632dbeb9144
App Academy Student <student@appacademy.io>
2014-08-04 15:34:45 -0700

    still not working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,4 @@
 class PagesController < ApplicationController
-  before_filter :allow_iframe_requests
-  
-  def allow_iframe_requests
-    response.headers.delete('X-Frame-Options')
-  end
-  
   def root
   end
   
