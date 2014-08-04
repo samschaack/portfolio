77b2178efaecab35d9a2fc022ead4cb99bd15e71
App Academy Student <student@appacademy.io>
2014-08-04 16:04:01 -0700

    all links working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,15 @@ class PagesController < ApplicationController
   
   def asteroids
   end
+  
+  def art
+  end
+  
+  def music
+  end
+  
+  def pdf
+    pdf_filename = File.join(Rails.root, "public/resume.pdf")
+    send_file(pdf_filename, :filename => "your_document.pdf", :disposition => 'inline', :type => "application/pdf")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/asteroids', to: 'pages#asteroids'
   get '/music', to: 'pages#music'
   get '/art', to: 'pages#art'
+  get '/resume', to: 'pages#pdf'
   root to: 'pages#root'
 end
