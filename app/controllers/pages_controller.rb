524b4d751eaa5d7708b68a48f934aaee70cf5f05
App Academy Student <student@appacademy.io>
2014-08-04 15:27:47 -0700

    another attempt at fixing things for heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  before_filter :allow_iframe_requests
+  
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
+  end
+  
   def root
   end
   
